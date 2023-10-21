1c2f2ade4c546c5bf3c2ca2d07ef8c446d394967
Marijana Djurica <djuricamj@gmail.com>
2023-10-21 19:49:34 +0200

    Add Basic Test

---
+++ itbootcamp_final_selenium_project/src/test/java/tests/BasicTest.java
@@ -0,0 +1,59 @@
+package tests;
+
+import Helper.Helper;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+
+public abstract class BasicTest {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+    protected String baseUrl = "https://vue-demo.daniel-avellaneda.com";
+
+
+    @BeforeClass
+    public void setUp () {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        driver.manage().deleteAllCookies();
+        driver.navigate().to(baseUrl);
+    }
+
+    @AfterMethod
+    public void afterMethod(ITestResult testResult) throws IOException {
+
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            Helper.takeScreenshotWithDateTimeFormat(driver, testResult.getName());
+        }
+    }
+
+    @AfterClass
+    public void afterClass () {
+        driver.quit();
+    }
+
+
+}
