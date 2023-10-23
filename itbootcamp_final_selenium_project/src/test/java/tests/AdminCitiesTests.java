fd56b7563aec003240d72a329f0cbc41328a4fd0
Marijana Djurica <djuricamj@gmail.com>
2023-10-23 18:07:38 +0200

    Add \Admin cities tests - Search city

--- itbootcamp_final_selenium_project/src/test/java/tests/AdminCitiesTests.java
+++ itbootcamp_final_selenium_project/src/test/java/tests/AdminCitiesTests.java
@@ -1,6 +1,8 @@
 package tests;
 
 import RetryAnalyzer.retry;
+import org.bouncycastle.crypto.agreement.srp.SRP6Client;
+import org.openqa.selenium.Keys;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -17,6 +19,7 @@ public class AdminCitiesTests extends BasicTest{
         navPage.waitUntilCurrentUrlContainsAadminCities();
         Assert.assertEquals(navPage.getCurrentUrl(), baseUrl + "/admin/cities",
                 "Current url should be " + baseUrl + "/admin/cities");
+
 
     }
 
@@ -52,8 +55,50 @@ public class AdminCitiesTests extends BasicTest{
         Assert.assertTrue(messagePopUpPage.getTextForSuccessfulAddOrEditCityMessage()
                 , "Popup message for successful add/edit citi should contains 'Saved successfully'");
 
+    }
+
+    @Test(priority = 4, retryAnalyzer = retry.class)
+    public void editCity () throws InterruptedException {
+        String oldCityName = "Marijana Djurica's city";
+        String newCityName = "Marijana Djurica's city Edited";
+
+      navPage.clickOnLoginButton();
+      loginPage.loginAsAdmin();
+      loginPage.clickOnLoginButton();
+      navPage.clickOnAdminButton();
+      navPage.clickOnCitiesButton();
+      navPage.waitUntilCurrentUrlContainsAadminCities();
+      citiesPage.getSearchInputBox().sendKeys(oldCityName);
+      citiesPage.waitUntilNumberOfRowsOfCitiesInSearchIsOne();
+      citiesPage.clickOnEditCityButton();
+      citiesPage.getNameInputBox().sendKeys(Keys.CONTROL,"a",Keys.DELETE);
+      Thread.sleep(4000);
+      citiesPage.getNameInputBox().sendKeys(newCityName);
+      Thread.sleep(2300);
+      citiesPage.clickOnSaveButton();
+      messagePopUpPage.waitUntilSuccessfulAddOrEditCityMessageIsVisible();
+        Assert.assertTrue(messagePopUpPage.getTextForSuccessfulAddOrEditCityMessage()
+                , "Popup message for successful add/edit citi should contains 'Saved successfully'");
 
     }
+    @Test(priority = 5, retryAnalyzer = retry.class)
+    public void searchCity (){
+        String cityName = "Marijana Djurica's city Edited";
+        navPage.clickOnLoginButton();
+        loginPage.loginAsAdmin();
+        loginPage.clickOnLoginButton();
+        navPage.clickOnAdminButton();
+        navPage.clickOnCitiesButton();
+        navPage.waitUntilCurrentUrlContainsAadminCities();
+        citiesPage.getSearchInputBox().sendKeys(cityName);
+        citiesPage.waitUntilNumberOfRowsOfCitiesInSearchIsOne();
+
+        Assert.assertEquals(citiesPage.getTextForCityName(), cityName,
+                "City name should be" + cityName);
+
+
+    }
+
 
 
 
