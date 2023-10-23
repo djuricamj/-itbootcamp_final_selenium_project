9523fc4d440404008d43e1cd6e46e758195bae6d
Marijana Djurica <djuricamj@gmail.com>
2023-10-23 19:34:25 +0200

    Add Locale tests - Set locale to EN

--- itbootcamp_final_selenium_project/src/test/java/tests/LocaleTests.java
+++ itbootcamp_final_selenium_project/src/test/java/tests/LocaleTests.java
@@ -1,9 +1,12 @@
 package tests;
 
+import RetryAnalyzer.retry;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class LocaleTests extends BasicTest{
 
+    @Test(priority = 1, retryAnalyzer = retry.class)
     public void  SetLocaleToEs (){
         navPage.clickOnLanguageButton();
         navPage.waitUntilLanguageListIsVisible();
@@ -11,6 +14,15 @@ public class LocaleTests extends BasicTest{
         Assert.assertEquals(navPage.getHeaderText(), "Página de aterrizaje",
                 "Text in header should be 'Página de aterrizaje'.");
 
+    }
+
+    @Test(priority = 2, retryAnalyzer = retry.class)
+    public void  SetLocaleToEn (){
+        navPage.clickOnLanguageButton();
+        navPage.waitUntilLanguageListIsVisible();
+        navPage.clickOnEnglishButton();
+        Assert.assertEquals(navPage.getHeaderText(), " Landing",
+                "Text in header should be ' Landing'.");
     }
 
 }
